b50efcdf6539aae31b927e465e6ec162078dd74c
reutMandel <reut.mandel.rm@gmail.com>
2020-12-18 17:59:15 +0200

    add functions in pstring

--- func_select.s
+++ func_select.s
@@ -1,9 +1,13 @@
     .section .rodata
-    .align 8 # Align address to multiple of 4
+format1:	  .string	"first pstring length: %d, second pstring length: %d\n"
+format_in: .string	" %c"
+format2:	  .string	"old char: %c, new char: %c, first string: %s, second string: %s\n"
+
+    .align 8 # Align address to multiple of 8
 .L7:
 
   .quad .L0 # case 50 or 60      
-  .quad .L6 # case 51
+  .quad .L6 # defualt
   .quad .L2 # case 52
   .quad .L3 # case 53
   .quad .L4 # case 54
@@ -13,23 +17,79 @@
 
 # case 50 or 60      
 .L0:
-
-   add $10, %eax
+   
+   # call pstrlen for first pstring
+   movq  %rsi,  %rdi    # move pstring 1 to rdi
+   movq  %rdx,  %rsi    # move pstring 2 to rsi
+   call pstrlen
+   
+   # call pstrlen for second pstring
+   movq  %rsi, %rdi     # move pstring 2 to rdi
+   movq  %rax, %rsi     # save size of pstring 1 in %rsi
+   call  pstrlen
+   movq  %rax, %rdx     # save size of pstring 2 in %rdx
+   
+   # printing the two length of the pstrings
+   movq	$format1, %rdi # the string is the first paramter passed to the printf function.
+   call  printf
    jmp .L8
 
 # case 52
 .L2:
-   add $20, %eax
+
+   movq %rsi, %rdi
+   movb $97,  %sil
+   movb $98,  %dl
+   call replaceChar
+
+  /* movq %rsi, %rcx        # move pstring 1 to %rcx
+   sub  $16, %rsp         # place for the char
+   
+   # call scanf for first char
+   movq $format_in, %rdi  # the string is the first paramter passed to the scanf function.
+   leaq -1(%rbp), %rsi    # leaq %rsp to %rsi 
+   movq $0, %rax          # initiliaze %rax
+   call scanf 
+
+   movb -1(%rbp), %bl     # insert scanf result to %bl
+
+   # call scanf for second char
+   movq $format_in, %rdi  # the string is the first paramter passed to the scanf function.
+   leaq -1(%rbp), %rsi    # leaq %rsp to %rsi 
+   movq $0, %rax          # initiliaze %rax
+   call scanf            
+   
+   # call to replace char
+   movq %rcx, %rdi        # move pstring 1 to %rdi
+   movb %sil, %dl         # move new char to %rdx
+   movb %bl,  %sil        # move old char to %rsi
+   movq $0, %rax          # initiliaze %rax
+   //call replaceChar
+
+   */
+   
+   # printing the new pstrings after replace char
+   movq  %rdi,  %rcx
+   movq	$format2,  %rdi # the string is the first paramter passed to the printf function.
+   call  printf
    jmp .L8
 
 # case 53
 .L3:
-   add $20, %eax  
+
+   movq  %rsi,  %rdi    # move pstring 1 to rdi
+   movq  %rdx,  %rsi    # move pstring 2 to rsi
+   movb  $2,    %dl
+   movb  $1,    %cl
+   call pstrijcpy
    jmp .L8
 
 # case 54
 .L4:
-   add $20, %eax
+
+   movq  %rsi,  %rdi    # move pstring 1 to rdi
+   movq  %rdx,  %rsi    # move pstring 2 to rsi
+   call  swapCase
    jmp .L8
 
 # case 55
--- pstring.s
+++ pstring.s
@@ -1,1 +1,191 @@
 .file "pstring.h"
+
+     .section .rodata
+format1:	  .string	"invalid input!\n"
+
+ .text	#the beginning of the code.
+
+.globl	pstrlen
+        #this function gets pstring and retrun its length
+	    .type  pstrlen, @function
+pstrlen:
+   
+   pushq  %rbp            #saving the old frame pointer.
+   movq   %rsp,  %rbp     #creating the new frame pointer.
+   xorb   %al,  %al       # initilaize al
+   movb   (%rdi), %al     # move rdi[0] to al
+   leave
+   ret
+
+.globl	replaceChar
+        #this function gets pstring and two chars and replace the old char in new char
+	    .type  replaceChar, @function
+replaceChar:
+   pushq  %rbp            # saving the old frame pointer.
+   movq   %rsp,  %rbp     # creating the new frame pointer.
+   xorq   %rax,  %rax     # initilaize rax
+   movl   $1,    %ecx     # initilaize ecx - index of the current iteration
+    
+    # loop until finish iterate on the string
+   .L1:
+      cmpb $0, (%rdi, %rcx, 1)   # comapre current char to '\0' 
+      jz   .L2                   # if equal - finished iteration - go to L2
+      cmpb %sil, (%rdi, %rcx, 1) # compare current char to old char (in %rsi)
+      je   .L3                   # if equal - replace char - go to L3
+      inc  %ecx                  # increment index in 1
+      jmp  .L1
+     
+    # finish label
+    .L2: 
+      movq %rdi, %rax            # mov the new pstring to rax
+      leave
+      ret
+    
+    # replace label
+    .L3:
+      movb %dl, (%rdi, %rcx, 1)  # replace the new char in pstring
+      inc  %ecx                  # increment index in 1
+      jmp  .L1                   # jump back to loop
+
+
+.globl	pstrijcpy
+        #this function gets two pstring and two indexes and copy the i-j in src pstring to i-j dest pstring
+	    .type  pstrijcpy , @function
+pstrijcpy:
+
+   pushq  %rbp            # saving the old frame pointer.
+   movq   %rsp,   %rbp    # creating the new frame pointer.
+   xorq   %rax,   %rax    # initilaize rax
+   movb   (%rdi), %r8b    # move rdi[0] to r8b
+   movb   (%rsi), %r9b    # move rsi[0] to r9b
+   cmpb    %r8b,   %dl    # compare i to size of pstring 1
+   ja     .L4             # if i above size - finished - go to L4 
+   cmpb    %r9b,   %dl    # compare i to size of pstring 2
+   ja     .L4             # if i above size - finished - go to L4 
+   cmpb    %r8b,   %cl    # compare j to size of pstring 1
+   ja     .L4             # if j above size - finished - go to L4 
+   cmpb    %r9b,   %cl    # compare j to size of pstring 2
+   ja     .L4             # if j above size - finished - go to L4 
+   cmpb    %cl,    %dl    # compare i and j
+   ja     .L4             # if i > j - finished - go to L4
+   xorq   %r8,     %r8    # initilaize r8
+   movb   %dl,     %r8b   # index iteration start in i
+   xorq   %r9,     %r9    # initilaize r9
+
+   .L5:
+
+      cmpb  %cl, %r8b             # compare index iteration to j
+      ja    .L6                   # if index interation above j - finishd - go to L6
+      movb  (%rdi,%r8,1), %r9b    # move the char in location %r8 in src to register %rb9  
+      movb  %r9b,  (%rsi,%r8,1)   # move the char in %r9 to the r8 index in dest pstring
+      inc   %r8                   # increment index in 1 
+      jmp   .L5
+
+   .L6:
+
+      movq %rsi, %rax     # mov dest pstring to rax
+      leave
+      ret
+
+   .L4:
+      
+      # call printf - invalid arguments i or j
+      movq $format1, %rdi # the string is the first paramter passed to the printf function.
+      call printf
+      jmp  .L6
+
+
+.globl	swapCase
+        #this function swap lower to upper case and upper to lower case
+	    .type  swapCase , @function
+swapCase:
+   
+   pushq  %rbp            # saving the old frame pointer.
+   movq   %rsp,   %rbp    # creating the new frame pointer.
+   xorq   %rax,   %rax    # initilaize rax
+   movl   $1,     %ecx    # initilaize ecx - index of the current iteration
+    
+    # loop until finish iterate on the string
+   .L7:
+      cmpb $0, (%rdi, %rcx, 1)   # comapre current char to '\0' 
+      jz   .L8                   # if equal - finished iteration - go to L8
+
+      # check big letter
+      .L10: 
+       cmpb $65, (%rdi, %rcx, 1)  # compare current char to 'A' (A=65)
+       jl   .L9                   # current char less than 'A' - continue in loop - go to L9
+       cmpb $90, (%rdi, %rcx, 1)  # compare current char to 'Z' (Z=90)
+       ja   .L11                  # current char bigger than 'Z' - move to check lower letter - go to L11
+       add  $32, (%rdi, %rcx, 1)  # add 32 to current char - replact to lower case
+       jmp  .L9
+
+      # check lower letter
+      .L11:
+       cmpb $97, (%rdi, %rcx, 1)  # compare current char to 'a' (a=97)
+       jl   .L9                   # current char less than 'a' - continue in loop - go to L9
+       cmpb $122, (%rdi, %rcx, 1) # compare current char to 'z' (z=122)
+       ja   .L9                   # current char bigger than 'z' - continue in loop - go to L9
+       sub  $32, (%rdi, %rcx, 1)  # sub 32 to current char - replact to upper case
+       jmp  .L9
+ 
+    # finish label
+   .L8: 
+      movq %rdi, %rax             # mov the new pstring to rax
+      leave
+      ret
+
+   #  increase index 
+   .L9: 
+      inc  %ecx                   # increase index
+      jmp .L7                     # back to loop
+
+
+.globl	pstrijcmp
+        #this function swap lower to upper case and upper to lower case
+	    .type  pstrijcmp , @function
+pstrijcmp:
+    
+   pushq  %rbp            # saving the old frame pointer.
+   movq   %rsp,   %rbp    # creating the new frame pointer.
+   xorq   %rax,   %rax    # initilaize rax
+   movb   (%rdi), %r8b    # move rdi[0] to r8b
+   movb   (%rsi), %r9b    # move rsi[0] to r9b
+   cmpb   %r8b,   %dl     # compare i to size of pstring 1
+   ja     .L12            # if i above size - finished - go to L12
+   cmpb   %r9b,   %dl     # compare i to size of pstring 2
+   ja     .L12            # if i above size - finished - go to L12
+   cmpb   %r8b,   %cl     # compare j to size of pstring 1
+   ja     .L12            # if j above size - finished - go to L12
+   cmpb   %r9b,   %cl     # compare j to size of pstring 2
+   ja     .L12            # if j above size - finished - go to L12
+   cmpb   %cl,    %dl     # compare i and j
+   ja     .L12            # if i > j - finished - go to L4
+   xorq   %r8,     %r8    # initilaize r8
+   movb   %dl,     %r8b   # index iteration start in i
+   xorq   %r9,     %r9    # initilaize r9
+
+  L13:
+
+
+     
+
+
+
+    
+
+  L12:
+      
+      # call printf - invalid arguments i or j
+      movq $format1, %rdi # the string is the first paramter passed to the printf function.
+      call printf
+    //  movb $-2  %al
+      jmp  .L13
+
+  .L13:
+
+     // movq %rsi, %rax     # mov dest pstring to rax
+      leave
+      ret
+
+
+   
