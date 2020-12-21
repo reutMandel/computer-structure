6f98c4ade62feb331f9189db3a8780ab17723e83
reutMandel <reut.mandel.rm@gmail.com>
2020-12-21 18:06:13 +0200

    tt

--- run_main.s
+++ run_main.s
@@ -1,5 +1,5 @@
 # 209163955 Reut Mandel
-# used Ron Even and Yanir Buznah tests
+# check with Ron Even and Yanir Buznah tests
  .section .rodata
 format_in_int: .string	    "%d"
 format_in_string: .string	"%s"
